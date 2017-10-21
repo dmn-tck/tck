216c574fcc270d142bf5e49ee53f7f542a9842af
Arthur H <516104+arthware@users.noreply.github.com>
2017-10-21 15:45:03 +0200

    Cleanup / Update marshaller module and consolidated testCase.xsd schema files (#72)
    * Clenaup/improvements to marshaller module (#58)
    
    * jaxB classes are generated based on TestCases/testCases.xsd schema file
    * Automated jaxb generation to always re-generate based on latest schema
    * No manual changes for generated code required anymore
    * Updated DroolsTckRunner to use latest generated jaxb model
    
    Generated jaxB classes have changed due to introduction of nilable in
    testCases.xsd
    
    * Removed redundand schema files (#58)

--- runners/dmn-tck-marshaller/src/main/java/org/omg/dmn/tck/marshaller/_20160719/json/ValueSerializer.java
+++ runners/dmn-tck-marshaller/src/main/java/org/omg/dmn/tck/marshaller/_20160719/json/ValueSerializer.java
@@ -1,20 +1,19 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package org.omg.dmn.tck.marshaller._20160719.json;
 
 import java.io.IOException;
+
+import javax.xml.bind.JAXBElement;
 
 import org.w3c.dom.Element;
 
@@ -23,18 +22,36 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-public class ValueSerializer extends JsonSerializer<Object> {
+public class ValueSerializer extends JsonSerializer<Object>
+{
 
-    @Override
-    public void serialize(Object value, JsonGenerator gen,
-            SerializerProvider serializers) throws IOException,
-            JsonProcessingException {
-        if (value instanceof Element) {
-            String content = ((Element) value).getTextContent();
-            gen.writeString(content);
-        }else {
-            gen.writeObject(value);
-        }
-    }
+   @Override
+   public void serialize(Object value, JsonGenerator gen,
+      SerializerProvider serializers) throws IOException,
+      JsonProcessingException
+   {
+      if (value instanceof Element)
+      {
+         Element element = (Element) value;
+         String content = element.getTextContent();
+         gen.writeString(content);
+      }
+      else if (value instanceof JAXBElement<?>)
+      {
+         Object elemValue = ((JAXBElement<?>) value).getValue();
+         if (elemValue instanceof Element)
+         {
+            serialize(((JAXBElement<?>) value).getValue(), gen, serializers);
+         }
+         else
+         {
+            gen.writeObject(elemValue);
+         }
+      }
+      else
+      {
+         gen.writeObject(value);
+      }
+   }
 
 }
--- runners/dmn-tck-marshaller/src/test/java/org/omg/dmn/tck/marshaller/_20160719/TestCasesTest.java
+++ runners/dmn-tck-marshaller/src/test/java/org/omg/dmn/tck/marshaller/_20160719/TestCasesTest.java
@@ -1,15 +1,12 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package org.omg.dmn.tck.marshaller._20160719;
@@ -27,7 +24,6 @@ import javax.xml.bind.Unmarshaller;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.omg.dmn.tck.marshaller._20160719.TestCases;
 import org.omg.dmn.tck.marshaller._20160719.TestCases.TestCase;
 import org.omg.dmn.tck.marshaller._20160719.TestCases.TestCase.InputNode;
 import org.w3c.dom.Element;
@@ -35,83 +31,101 @@ import org.w3c.dom.Element;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SuppressWarnings("restriction")
-public class TestCasesTest {
+public class TestCasesTest
+{
 
-    private static ObjectMapper objectMapper;
+   private static ObjectMapper objectMapper;
 
-    private static File baseOutputDir;
+   private static File baseOutputDir;
 
-    private static File level2Dir;
+   private static File level2Dir;
 
-    @BeforeClass
-    public static void setUpClass() {
-        objectMapper = new ObjectMapper();
+   @BeforeClass
+   public static void setUpClass()
+   {
+      objectMapper = new ObjectMapper();
 
-        baseOutputDir = new File("target", "test-classes");
-        baseOutputDir.mkdirs();
+      baseOutputDir = new File("target", "test-classes");
+      baseOutputDir.mkdirs();
 
-        level2Dir = new File(baseOutputDir, "compliance-level-2");
-        level2Dir.mkdirs();
-    }
+      level2Dir = new File(baseOutputDir, "compliance-level-2");
+      level2Dir.mkdirs();
+   }
 
-    @Test
-    public void testRead0001() throws Exception {
-        InputStream is = null;
-        try {
-            is = getClass()
-                    .getResourceAsStream(
-                            "/compliance-level-2/0001-input-data-string/0001-input-data-string-test-01.xml");
-            TestCases testCases = load(is);
-            assertNotNull(testCases);
-            assertEquals(2, testCases.getTestCase().size());
-            for (TestCase testCase : testCases.getTestCase()) {
-                for (InputNode inputNode : testCase.getInputNode()) {
-                    if (inputNode.getValue() != null) {
-                        System.out
-                                .println("inputNode is simple type with content: "
-                                        + ((Element) inputNode.getValue())
-                                                .getTextContent());
-                    } else {
-                        System.out.println("inputNode is complex: "
-                                + inputNode.getComponent());
-                    }
-                }
+   @Test
+   public void testRead0001() throws Exception
+   {
+      InputStream is = null;
+      try
+      {
+         is = getClass()
+            .getResourceAsStream(
+               "/compliance-level-2/0001-input-data-string/0001-input-data-string-test-01.xml");
+         TestCases testCases = load(is);
+         assertNotNull(testCases);
+         assertEquals(2, testCases.getTestCase().size());
+         for (TestCase testCase : testCases.getTestCase())
+         {
+            for (InputNode inputNode : testCase.getInputNode())
+            {
+               if (inputNode.getValue() != null)
+               {
+                  System.out
+                     .println("inputNode is simple type with content: "
+                        + ((Element) inputNode.getValue().getValue())
+                           .getTextContent());
+               }
+               else
+               {
+                  System.out.println("inputNode is complex: "
+                     + inputNode.getComponent());
+               }
             }
+         }
 
-            String json = objectMapper.writeValueAsString(testCases);
-            // assertions on JSON go here
+         String json = objectMapper.writeValueAsString(testCases);
+         // assertions on JSON go here
 
-            File testOutputDir = new File(baseOutputDir,
-                    "0001-input-data-string");
-            testOutputDir.mkdirs();
-            File jsonFile = new File(testOutputDir,
-                    "0001-input-data-string-test-01.json");
-            System.out.println("Writing JSON output to: "
-                    + jsonFile.getAbsolutePath());
-            objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonFile,
-                    testCases);
+         File testOutputDir = new File(baseOutputDir,
+            "0001-input-data-string");
+         testOutputDir.mkdirs();
+         File jsonFile = new File(testOutputDir,
+            "0001-input-data-string-test-01.json");
+         System.out.println("Writing JSON output to: "
+            + jsonFile.getAbsolutePath());
+         objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonFile,
+            testCases);
 
-        } finally {
-            try {
-                is.close();
-            } catch (Exception e) {
-                ;
-            }
-        }
-    }
+      }
+      finally
+      {
+         try
+         {
+            is.close();
+         }
+         catch (Exception e)
+         {
+            ;
+         }
+      }
+   }
 
-    @SuppressWarnings({ "unchecked" })
-    protected final TestCases load(InputStream inputStream)
-            throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(TestCases.class);
-        Unmarshaller um = context.createUnmarshaller();
+   @SuppressWarnings({ "unchecked" })
+   protected final TestCases load(InputStream inputStream)
+      throws JAXBException
+   {
+      JAXBContext context = JAXBContext.newInstance(TestCases.class);
+      Unmarshaller um = context.createUnmarshaller();
 
-        Object obj = um.unmarshal(inputStream);
-        if (obj instanceof JAXBElement<?>) {
-            return ((JAXBElement<TestCases>) obj).getValue();
-        } else {
-            return (TestCases) obj;
-        }
-    }
+      Object obj = um.unmarshal(inputStream);
+      if (obj instanceof JAXBElement<?>)
+      {
+         return ((JAXBElement<TestCases>) obj).getValue();
+      }
+      else
+      {
+         return (TestCases) obj;
+      }
+   }
 
 }
