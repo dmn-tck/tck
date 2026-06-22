77dbf84a83a2f85ad4783aa365f52fb237deea14
athira <athira77@ibm.com>
2026-06-22 11:56:24 +0530

    code refactoring

--- runners/dmn-tck-runner-drools/src/test/java/org/omg/dmn/tck/runner/drools/CompareValuesUtil.java
+++ runners/dmn-tck-runner-drools/src/test/java/org/omg/dmn/tck/runner/drools/CompareValuesUtil.java
@@ -3,35 +3,63 @@ package org.omg.dmn.tck.runner.drools;
 import org.kie.dmn.feel.util.NumberEvalHelper;
 
 import java.math.BigDecimal;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 public final class CompareValuesUtil {
 
     public static final BigDecimal NUMBER_COMPARISON_PRECISION = new BigDecimal("0.00000001");
 
     public static boolean areEqual(Object object1, Object object2) {
-        // This includes both being null.
-        if (object1 == object2) {
-            return true;
-            // If one of those is null.
-        } else if ((object1 == null) || (object2 == null)) {
-            return false;
-        } else if (object1 instanceof Number && object2 instanceof Number) {
-            BigDecimal expectedBD = NumberEvalHelper.getBigDecimalOrNull(object1);
-            BigDecimal actualBD = NumberEvalHelper.getBigDecimalOrNull(object2);
-            return expectedBD.subtract(actualBD).abs().compareTo(NUMBER_COMPARISON_PRECISION) < 0;
-        } else if (object1 instanceof List && object2 instanceof List) {
-            return areEqualLists((List<Object>) object1, (List<Object>) object2);
-        } else if (object1 instanceof Map && object2 instanceof Map) {
-            return areEqualMaps((Map<Object, Object>) object1, (Map<Object, Object>) object2);
-        } else if (object1 instanceof String || object2 instanceof String) {
-            return object1.toString().equals(object2.toString());
-        } else if (!object1.getClass().isAssignableFrom(object2.getClass())) {
-            return false;
-        } else {
-            return object1.equals(object2);
+        Map<BiPredicate<Object, Object>, BiFunction<Object, Object, Boolean>> comparators = getEqualityComparators();
+        
+        for (Map.Entry<BiPredicate<Object, Object>, BiFunction<Object, Object, Boolean>> entry : comparators.entrySet()) {
+            if (entry.getKey().test(object1, object2)) {
+                return entry.getValue().apply(object1, object2);
+            }
         }
+        return Objects.equals(object1, object2);
+    }
+
+    private static Map<BiPredicate<Object, Object>, BiFunction<Object, Object, Boolean>> getEqualityComparators() {
+        Map<BiPredicate<Object, Object>, BiFunction<Object, Object, Boolean>> map = new LinkedHashMap<>();
+        
+        map.put((o1, o2) -> o1 == o2, (o1, o2) -> true);
+        
+        map.put((o1, o2) -> o1 == null || o2 == null, (o1, o2) -> false);
+        
+        map.put(
+            (o1, o2) -> o1 instanceof Number && o2 instanceof Number,
+            (o1, o2) -> {
+                BigDecimal expectedBD = NumberEvalHelper.getBigDecimalOrNull(o1);
+                BigDecimal actualBD = NumberEvalHelper.getBigDecimalOrNull(o2);
+                return expectedBD.subtract(actualBD).abs().compareTo(NUMBER_COMPARISON_PRECISION) < 0;
+            }
+        );
+        
+        map.put(
+            (o1, o2) -> o1 instanceof List && o2 instanceof List,
+            (o1, o2) -> areEqualLists((List<Object>) o1, (List<Object>) o2));
+
+        map.put(
+            (o1, o2) -> o1 instanceof Map && o2 instanceof Map,
+            (o1, o2) -> areEqualMaps((Map<Object, Object>) o1, (Map<Object, Object>) o2)
+        );
+
+        map.put(
+            (o1, o2) -> o1 instanceof String || o2 instanceof String,
+            (o1, o2) -> o1.toString().equals(o2.toString()));
+
+        map.put(
+            (o1, o2) -> !o1.getClass().isAssignableFrom(o2.getClass()),
+            (o1, o2) -> false
+        );
+        
+        return map;
     }
 
     private static boolean areEqualLists(List<Object> list1, List<Object> list2) {
